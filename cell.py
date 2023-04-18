855392b7db2e2f1a0e254edb0782627be667a740
myakhatri <127243859+myakhatri@users.noreply.github.com>
2023-04-17 22:05:57 -0400

    Create cell.py

---
+++ cell.py
@@ -0,0 +1,17 @@
+class Cell:
+  def __init__(self, value, row, col, screen):
+    self.value = value
+    self.row = row
+    self.col = col
+    self.screen = screen
+
+
+  def set_cell_value(self, value):
+    self.value = value
+    
+  def set_sketched_value(self,value):
+    self.sketched_value = value 
+  
+  def draw(self):
+    
+  
