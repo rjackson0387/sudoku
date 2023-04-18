793ee684d8301f2ea809d1fbdaf5aa489257c319
rjackson0387 <103905028+rjackson0387@users.noreply.github.com>
2023-04-17 22:05:23 -0400

    Create board.py

---
+++ board.py
@@ -0,0 +1,35 @@
+from cell import Cell
+
+
+
+
+
+
+class Board:
+  def __init__(self, width, height, screen, difficulty):
+    self.width = width
+    self.height = height
+    self.screen = screen
+    self.difficulty = difficulty
+    
+  def draw(self):
+    pass
+
+  def select(self,row, col):
+    pass 
+  def click(self, x, y):
+
+  def sketch(self, value):
+
+  def place_number(self, value):
+
+  def reset_to_original(self):
+
+  def is_full(self):
+
+  def update_board(self):
+
+  def find_empty(self):
+
+  def check_board(self):
+  
