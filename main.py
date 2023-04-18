2930a291eec0fab16041ef53f271403f749f3ce4
jaz <jazlyndailey03@gmail.com>
2023-04-18 19:42:00 -0400

    board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from board import Board
-from sudoku_generator import SudokuGenerator
+import sudoku_generator
 
-
-
-
+board = sudoku_generator.SudokuGenerator(0)
+board.print_board()
+board[0][1] = 1
