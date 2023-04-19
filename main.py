665f1abf3095c3cb69779685649a3e47f5c30ecc
rjackson0387 <ross.jackson@ufl.edu>
2023-04-18 20:35:13 -0400

    Fill_diagonal works

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from sudoku_generator import SudokuGenerator
 
 board = SudokuGenerator(0)
-SudokuGenerator.print_board(board)
+#SudokuGenerator.print_board(board)
 # board[0][1] = 1
-print(board.fill_box(0,0))
+#print(board.fill_box(0,0))
+#SudokuGenerator.print_board(board)
+board.fill_diagonal()
 SudokuGenerator.print_board(board)
