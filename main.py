93392cb3dd4945f4b0a8d87a8fff4546c3b8a86a
rjackson0387 <ross.jackson@ufl.edu>
2023-04-19 19:49:51 -0400

    Fixed in remaining

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import sudoku_generator
 from sudoku_generator import SudokuGenerator
 import random
-'''board = SudokuGenerator(5)
+board = SudokuGenerator(9, 0)
 SudokuGenerator.print_board(board)
+print()
 # board[0][1] = 1
 
-print(board.is_valid(0,0,0))
+print(board.is_valid(0,3,1))
 
 
 board.fill_values()
 SudokuGenerator.print_board(board)
 
-board.remove_cells()
+print(board.valid_in_box(0,3,1))
+print(board.valid_in_col(3,1))
+print(board.valid_in_row(0,1))
+print(board.is_valid(0,3,1))
+#board.remove_cells()
 print()
 
-board.remove_cells()
+'''board.remove_cells()
 SudokuGenerator.print_board(board)
-'''
-board = sudoku_generator.generate_sudoku(9.30)
-print(board)
+print()'''
+
+board = sudoku_generator.generate_sudoku(9, 30)
+
