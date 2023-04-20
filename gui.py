cda05403260f2a92524501f7e372c70fa45a091f
rjackson0387 <ross.jackson@ufl.edu>
2023-04-19 21:02:01 -0400

    Dont use this one

--- gui.py
+++ gui.py
@@ -1,14 +1,24 @@
 import pygame
 from constants import *
 
-pygame.init()
+'''pygame.init()
 pygame.display.set_caption('Sudoku')
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+
+
+def draw_lines():
+    # draw horizontal lines
+    for i in range(1, BOARD_ROWS):
+        pygame.draw.line(screen,LINE_COLOR, 0, (i * SQUARE_SIZE, 0), (SQUARE_SIZE * i, HEIGHT), LINE_WIDTH)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen.fill(BG_COLOR)
+draw_lines()
 
 run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
+            run = False'''
 
