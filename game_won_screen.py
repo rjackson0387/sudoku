af5504c39f1bcd4f99623d2bca4c485afd8ffbfe
myakhatri <127243859+myakhatri@users.noreply.github.com>
2023-04-23 22:13:58 -0400

    game won

--- game_won_screen.py
+++ game_won_screen.py
@@ -1,1 +1,40 @@
+import pygame
+from constants import *
+
+#main menu
+pygame.init()
+pygame.display.set_caption('Sudoku')
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+Game_Won = "Game Won!"
+font = pygame.font.Font(None, 40)
+
+screen.fill((205,96,144))
+text_surface = font.render(Game_Won, True, (0, 0, 0))
+text_rect = text_surface.get_rect()
+text_rect.center = (WIDTH // 2, HEIGHT // 2 - 70)
+screen.blit(text_surface, text_rect)
+
+
+middle_button_rect = pygame.Rect(180, 280, 100, 40)
+middle_text = font.render('MEDIUM', True, (0, 0, 0))
+
+
+#middle button
+
+pygame.draw.rect(screen, (248,248,255), (middle_button_rect))
+text_rect = middle_text.get_rect(center=middle_button_rect.center)
+screen.blit(middle_text, text_rect)
+
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+
+
+
+
 
