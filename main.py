e1e9e45184144a9af9ac10379b6df15902caabfb
rjackson0387 <ross.jackson@ufl.edu>
2023-04-24 22:24:24 -0400

    Change game_board chagnes

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ while True: #New while loop
     difficulty = start_screen()
     restart_key = 0
     #game_board is the list with 0s, game_board_orig is the original list with no cells removed (aka the answer)
-    game_board, game_board_orig = sudoku_generator.generate_sudoku(9, difficulty)
+    game_board, game_board_orig = sudoku_generator.generate_sudoku(9, 1)
     for row, list in enumerate(game_board):
         for col, item in enumerate(list):
             Cell(item, col, row, 50, 50, board.screen)
@@ -176,8 +176,13 @@ while True: #New while loop
                         if item.cell.collidepoint(*coords):
                             if item.sketched_value in [1,2,3,4,5,6,7,8,9]:
                                 sudoku.place_number(item, item.sketched_value, *coords, game_board)
+                                print(game_board)
                 if sudoku.is_full(game_board):
-                    gamelostscreen()
+                    print("done")
+                    if sudoku.check_board(game_board, game_board_orig) == False:
+                        gamelostscreen()
+                    elif sudoku.check_board(game_board, game_board_orig) == True:
+                       print("w")
         pygame.display.update()
         if restart_key == 1:
             break
